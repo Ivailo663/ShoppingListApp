bc38568fbe8cc2c9ab2604cd77bcd4dc11f1cd7a
Ivailo663 <42377328+Ivailo663@users.noreply.github.com>
2020-07-07 00:24:52 +0300

    asd

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 3002;
 var path = require('path');
 app.use(express.static("./dist", { index: "index.html" }))
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist/index.html'))
-})
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'dist/index.html'))
+// })
 app.use('/static', express.static(path.join(__dirname, 'src/styles/gallery/')))
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
